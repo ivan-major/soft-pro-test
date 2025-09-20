86c95c713b99337a790e4695446a355dde4d8b99
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2025-09-20 13:04:27 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppContainer from '@/shared/ui/Container/AppContainer.vue'
+</script>
+
+<template>
+    <div class="bg-zinc-800">
+        <AppContainer>
+            <RouterView />
+        </AppContainer>
+    </div>
+    <MediaModal />
+</template>
